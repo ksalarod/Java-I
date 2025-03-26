baeef7dd00779097aaf2a2540ff1466ad33f96d1
Karla Salamanca Rodriguez <156261888+ksalarod@users.noreply.github.com>
2025-03-26 13:30:39 -0400

    Create PiFifteenTimes.java

---
+++ PiFifteenTimes.java
@@ -0,0 +1,21 @@
+import java.lang.Math;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.printf("PI(2) is %.2f%n", Math.PI);
+        System.out.printf("PI(3) is %.3f%n", Math.PI);
+        System.out.printf("PI(4) is %.4f%n", Math.PI);
+        System.out.printf("PI(5) is %.5f%n", Math.PI);
+        System.out.printf("PI(6) is %.6f%n", Math.PI);
+        System.out.printf("PI(7) is %.7f%n", Math.PI);
+        System.out.printf("PI(8) is %.8f%n", Math.PI);
+        System.out.printf("PI(9) is %.9f%n", Math.PI);
+        System.out.printf("PI(10) is %.10f%n", Math.PI);
+        System.out.printf("PI(11) is %.11f%n", Math.PI);
+        System.out.printf("PI(12) is %.12f%n", Math.PI);
+        System.out.printf("PI(13) is %.13f%n", Math.PI);
+        System.out.printf("PI(14) is %.14f%n", Math.PI);
+        System.out.printf("PI(15) is %.15f%n", Math.PI);
+    }
+}
